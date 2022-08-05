92561c77d00e53af41e08acd6ce337616772118c
Emeline <e.goll@hotmail.fr>
2022-08-05 16:46:13 +0200

    étape 14 - PouchDB

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { from, map, Observable } from 'rxjs';
 import { Product } from '../shared/product.model';
+import PouchDB from 'pouchdb-browser'
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,34 @@ setSelectedProducts(products: Product[]): void {
   this.selectedProducts = products;
 }
 
-  constructor(private httpClient: HttpClient) { }
+private localDb;
+
+constructor() {
+  this.localDb = new PouchDB('phones');
+  const remoteDb = new PouchDB(
+    'https://6a59157b-430d-4969-b802-b9c12470dafb-bluemix.cloudantnosqldb.appdomain.cloud/phones',
+  );
+  this.localDb.replicate.from(remoteDb, {
+    live: true,
+    retry: true,
+  });
+}
+
+getAllDocs(): Observable<Product[]> {
+  return from(
+    this.localDb.allDocs({
+      include_docs: true,
+    }),
+  ).pipe<Product[]>(
+    map((data: any) => data.rows.map((r: any) => r.doc)),
+  );
+}
+
+getDocById(id: string): Observable<Product> {
+  return from(this.localDb.get(id) as Promise<Product>);
+}
+
+  /*constructor(private httpClient: HttpClient) { }
 
   getAllDocs(): Observable<Product[]> {
     const result = this.httpClient.get<Product[]>('https://6a59157b-430d-4969-b802-b9c12470dafb-bluemix.cloudantnosqldb.appdomain.cloud/phones/_all_docs?include_docs=true');
@@ -34,7 +62,7 @@ setSelectedProducts(products: Product[]): void {
 
   getDocById(product_id: string): Observable<Product> {
     return this.httpClient.get<Product>(`https://6a59157b-430d-4969-b802-b9c12470dafb-bluemix.cloudantnosqldb.appdomain.cloud/phones/${product_id}`);
-  }
+  }*/
 
 
 }
