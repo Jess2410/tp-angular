d791d46eb30f3aa8e0bb81a229236c8a865474ba
Jessica <jess2410@hotmail.fr>
2022-08-05 11:56:06 +0200

    Etape 12 : page détail en cours

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'st-product-details',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
